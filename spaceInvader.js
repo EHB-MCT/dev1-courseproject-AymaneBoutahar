f81504bc86ee85175a865f459b4a3d1b58b2d2a0
Aymane Boutahar <aymane.boutahar@student.ehb.be>
2024-10-10 11:56:39 +0200

    SpaceInvader remade thanks to feedback

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,47 +1,45 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+let context = canvas.getContext("2d");
 
 signature();
 
 function signature() {
-	let canvas = document.querySelector("canvas");
-	canvas.width = 300;
-	canvas.height = 300;
-	let context = canvas.getContext("2d");
-
 	context.fillStyle = "black";
 	context.fillRect(0, 0, 300, 300);
 
 	context.fillStyle = "#4ee44e";
 
-	function drawSpaceInvaderchunk(x, y) {
-		context.beginPath();
-		context.moveTo(x, y);
-		context.lineTo(x + 50, y);
-		context.lineTo(x + 50, y + 50);
-		context.lineTo(x, y + 50);
-		context.closePath();
-		context.fill();
-	}
+	drawSpaceInvaderchunk(75, 25);
+	drawSpaceInvaderchunk(175, 25);
+	drawSpaceInvaderchunk(25, 25);
+	drawSpaceInvaderchunk(225, 25);
 
-	drawSpaceInvaderchunk(200, 50);
-	drawSpaceInvaderchunk(50, 50);
-	drawSpaceInvaderchunk(0, 50);
-	drawSpaceInvaderchunk(250, 50);
+	drawSpaceInvaderchunk(75, 75);
+	drawSpaceInvaderchunk(175, 75);
 
-	drawSpaceInvaderchunk(50, 100);
-	drawSpaceInvaderchunk(200, 100);
+	drawSpaceInvaderchunk(75, 125);
+	drawSpaceInvaderchunk(175, 125);
+	drawSpaceInvaderchunk(25, 125);
+	drawSpaceInvaderchunk(225, 125);
+	drawSpaceInvaderchunk(100, 125);
+	drawSpaceInvaderchunk(150, 125);
 
-	drawSpaceInvaderchunk(50, 150);
-	drawSpaceInvaderchunk(200, 150);
-	drawSpaceInvaderchunk(0, 150);
-	drawSpaceInvaderchunk(250, 150);
-	drawSpaceInvaderchunk(100, 150);
-	drawSpaceInvaderchunk(150, 150);
+	drawSpaceInvaderchunk(75, 225);
+	drawSpaceInvaderchunk(175, 225);
+	drawSpaceInvaderchunk(25, 225);
+	drawSpaceInvaderchunk(225, 225);
+}
 
-	drawSpaceInvaderchunk(50, 250);
-	drawSpaceInvaderchunk(200, 250);
-	drawSpaceInvaderchunk(0, 250);
-	drawSpaceInvaderchunk(250, 250);
+function drawSpaceInvaderchunk(x, y) {
+	context.beginPath();
+	context.moveTo(x, y);
+	context.lineTo(x + 50, y);
+	context.lineTo(x + 50, y + 50);
+	context.lineTo(x, y + 50);
+	context.closePath();
+	context.fill();
 }
